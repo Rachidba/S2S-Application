adc395c07fb9782a8cf79e260f38ed4a625ec87c
rachidba <rachidbaaziz.contact@gmail.com>
2018-11-21 15:53:09 +0000

    Create spring boot projetc

---
+++ s2s_springboot_api/src/test/java/io/rachidba/s2s/S2sApplicationTests.java
@@ -0,0 +1,16 @@
+package io.rachidba.s2s;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class S2sApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
